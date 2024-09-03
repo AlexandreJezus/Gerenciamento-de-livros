debfc60bcde7cebb9d91c5ae619963fb1560282e
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-09-03 09:31:28 -0300

    feat: models

--- src/models/estudante_model.js
+++ src/models/estudante_model.js
@@ -1,13 +1,23 @@
 let nextID = 1;
 
 const model = (estudante, id_estudante = nextID++) => {
-  if (estudante.nome != undefined && estudante.nome != "") {
+  if (
+    estudante.nome != undefined &&
+    estudante.nome != "" &&
+    estudante.matricula != undefined &&
+    estudante.matricula != "" &&
+    estudante.curso != undefined &&
+    estudante.curso != "" &&
+    estudante.ano != undefined &&
+    estudante.ano != "" &&
+    estudante.ano != isNaN
+  ) {
     return {
       id_estudante,
       nome: estudante.nome,
       matricula: estudante.matricula,
-      ano: estudante.ano,
       curso: estudante.curso,
+      ano: estudante.ano,
     };
   }
 };
--- src/models/livro_model.js
+++ src/models/livro_model.js
@@ -1,7 +1,17 @@
 let nextID = 1;
 
 const model = (livro, id_livro = nextID++) => {
-  if (livro.nome != undefined && livro.nome != "") {
+  if (
+    livro.titulo != undefined &&
+    livro.titulo != "" &&
+    livro.autor != undefined &&
+    livro.autor != "" &&
+    livro.ano != undefined &&
+    livro.ano != "" &&
+    livro.ano != isNaN &&
+    livro.genero != undefined &&
+    livro.genero != ""
+  ) {
     return {
       id_livro,
       titulo: livro.titulo,
